907fe98807571bb0f56d31ad921655e26c1325b5
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-26 21:37:06 +0530

    add ActivateBudgetHandler for activating budgets with DTO support

---
+++ modules/budget-management/application/commands/activate-budget.command.ts
@@ -0,0 +1,15 @@
+import { BudgetService } from '../services/budget.service'
+import { Budget } from '../../domain/entities/budget.entity'
+
+export interface ActivateBudgetDto {
+  budgetId: string
+  workspaceId: string
+}
+
+export class ActivateBudgetHandler {
+  constructor(private readonly budgetService: BudgetService) {}
+
+  async handle(dto: ActivateBudgetDto): Promise<Budget> {
+    return await this.budgetService.activateBudget(dto.budgetId, dto.workspaceId)
+  }
+}
