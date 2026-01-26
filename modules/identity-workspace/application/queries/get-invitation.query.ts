7605f57acb4a6dd58daf36079407fc1771aad9fa
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-26 17:34:53 +0530

    add GetInvitation queries and handlers for managing workspace invitations

---
+++ modules/identity-workspace/application/queries/get-invitation.query.ts
@@ -0,0 +1,50 @@
+import { WorkspaceInvitationService } from '../services/workspace-invitation.service'
+import { WorkspaceInvitation } from '../../domain/entities/workspace-invitation.entity'
+
+export interface GetInvitationByTokenQuery {
+  token: string
+}
+
+export interface GetWorkspaceInvitationsQuery {
+  workspaceId: string
+}
+
+export interface GetPendingInvitationsQuery {
+  workspaceId: string
+}
+
+export interface GetUserInvitationsQuery {
+  email: string
+}
+
+export class GetInvitationByTokenHandler {
+  constructor(private readonly invitationService: WorkspaceInvitationService) {}
+
+  async handle(query: GetInvitationByTokenQuery): Promise<WorkspaceInvitation | null> {
+    return await this.invitationService.getInvitationByToken(query.token)
+  }
+}
+
+export class GetWorkspaceInvitationsHandler {
+  constructor(private readonly invitationService: WorkspaceInvitationService) {}
+
+  async handle(query: GetWorkspaceInvitationsQuery): Promise<WorkspaceInvitation[]> {
+    return await this.invitationService.getWorkspaceInvitations(query.workspaceId)
+  }
+}
+
+export class GetPendingInvitationsHandler {
+  constructor(private readonly invitationService: WorkspaceInvitationService) {}
+
+  async handle(query: GetPendingInvitationsQuery): Promise<WorkspaceInvitation[]> {
+    return await this.invitationService.getPendingInvitations(query.workspaceId)
+  }
+}
+
+export class GetUserInvitationsHandler {
+  constructor(private readonly invitationService: WorkspaceInvitationService) {}
+
+  async handle(query: GetUserInvitationsQuery): Promise<WorkspaceInvitation[]> {
+    return await this.invitationService.getUserInvitations(query.email)
+  }
+}
