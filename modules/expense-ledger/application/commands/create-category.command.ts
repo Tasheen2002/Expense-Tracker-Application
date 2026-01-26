e96d2afc4a933ab39bce16040d83f8f38d173211
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-26 17:28:25 +0530

    add CreateCategory command and handler for category management

---
+++ modules/expense-ledger/application/commands/create-category.command.ts
@@ -0,0 +1,23 @@
+export class CreateCategoryCommand {
+  constructor(
+    public readonly workspaceId: string,
+    public readonly name: string,
+    public readonly description?: string,
+    public readonly color?: string,
+    public readonly icon?: string
+  ) {}
+}
+
+export class CreateCategoryHandler {
+  constructor(private readonly categoryService: any) {}
+
+  async handle(command: CreateCategoryCommand) {
+    return await this.categoryService.createCategory({
+      workspaceId: command.workspaceId,
+      name: command.name,
+      description: command.description,
+      color: command.color,
+      icon: command.icon,
+    })
+  }
+}
