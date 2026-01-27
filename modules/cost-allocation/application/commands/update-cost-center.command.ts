c58815780ac1253ad40e781c4bec207514b9cc37
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 01:40:00 +0530

    add UpdateCostCenterCommand and handler for updating cost centers

---
+++ modules/cost-allocation/application/commands/update-cost-center.command.ts
@@ -0,0 +1,28 @@
+import { AllocationManagementService } from '../services/allocation-management.service'
+import { CostCenter } from '../../domain/entities/cost-center.entity'
+
+export class UpdateCostCenterCommand {
+  constructor(
+    public readonly id: string,
+    public readonly workspaceId: string,
+    public readonly name?: string,
+    public readonly code?: string,
+    public readonly description?: string | null
+  ) {}
+}
+
+export class UpdateCostCenterHandler {
+  constructor(
+    private readonly allocationManagementService: AllocationManagementService
+  ) {}
+
+  async handle(command: UpdateCostCenterCommand): Promise<CostCenter> {
+    return await this.allocationManagementService.updateCostCenter({
+      id: command.id,
+      workspaceId: command.workspaceId,
+      name: command.name,
+      code: command.code,
+      description: command.description,
+    })
+  }
+}
