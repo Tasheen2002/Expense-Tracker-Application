6ca399695580f0db4e44b611501b8124b7f78242
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-27 20:43:13 +0530

    add index file for notification dispatch application queries

---
+++ modules/notification-dispatch/application/queries/index.ts
@@ -0,0 +1,5 @@
+export * from "./list-notifications.query";
+export * from "./get-unread-notifications.query";
+export * from "./get-unread-count.query";
+export * from "./get-preferences.query";
+export * from "./get-active-template.query";
