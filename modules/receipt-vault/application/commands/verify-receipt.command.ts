19a6ea50871e9eb7920b72bc9131d4e30bc6e84c
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-27 10:30:29 +0530

    add VerifyReceipt command and handler for verifying receipts

---
+++ modules/receipt-vault/application/commands/verify-receipt.command.ts
@@ -0,0 +1,15 @@
+import { ReceiptService } from '../services/receipt.service'
+import { Receipt } from '../../domain/entities/receipt.entity'
+
+export interface VerifyReceiptDto {
+  receiptId: string
+  workspaceId: string
+}
+
+export class VerifyReceiptHandler {
+  constructor(private readonly receiptService: ReceiptService) {}
+
+  async handle(dto: VerifyReceiptDto): Promise<Receipt> {
+    return await this.receiptService.verifyReceipt(dto.receiptId, dto.workspaceId)
+  }
+}
