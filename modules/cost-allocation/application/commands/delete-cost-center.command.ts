a54c5d13a76ff69e62f5372794bed27ac55b6f5a
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 01:39:31 +0530

    add DeleteCostCenterCommand and handler for deleting cost centers

---
+++ modules/cost-allocation/application/commands/delete-cost-center.command.ts
@@ -0,0 +1,21 @@
+import { AllocationManagementService } from '../services/allocation-management.service'
+
+export class DeleteCostCenterCommand {
+  constructor(
+    public readonly id: string,
+    public readonly workspaceId: string
+  ) {}
+}
+
+export class DeleteCostCenterHandler {
+  constructor(
+    private readonly allocationManagementService: AllocationManagementService
+  ) {}
+
+  async handle(command: DeleteCostCenterCommand): Promise<void> {
+    await this.allocationManagementService.deleteCostCenter(
+      command.id,
+      command.workspaceId
+    )
+  }
+}
