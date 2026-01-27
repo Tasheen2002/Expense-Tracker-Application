bdddef8e0a57261d542e800a2e3ba0a6ffa1819c
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-27 20:42:48 +0530

    add command exports for notification dispatch module

---
+++ modules/notification-dispatch/application/commands/index.ts
@@ -0,0 +1,5 @@
+export * from "./send-notification.command";
+export * from "./mark-as-read.command";
+export * from "./mark-all-as-read.command";
+export * from "./update-preferences.command";
+export * from "./create-template.command";
