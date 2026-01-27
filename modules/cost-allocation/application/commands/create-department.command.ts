a4d42795e0d14376acfba90a51b8457b321b6532
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 01:38:58 +0530

    add CreateDepartmentCommand and handler for creating departments

---
+++ modules/cost-allocation/application/commands/create-department.command.ts
@@ -0,0 +1,30 @@
+import { AllocationManagementService } from '../services/allocation-management.service'
+import { Department } from '../../domain/entities/department.entity'
+
+export class CreateDepartmentCommand {
+  constructor(
+    public readonly workspaceId: string,
+    public readonly name: string,
+    public readonly code: string,
+    public readonly description?: string,
+    public readonly managerId?: string,
+    public readonly parentDepartmentId?: string
+  ) {}
+}
+
+export class CreateDepartmentHandler {
+  constructor(
+    private readonly allocationManagementService: AllocationManagementService
+  ) {}
+
+  async handle(command: CreateDepartmentCommand): Promise<Department> {
+    return await this.allocationManagementService.createDepartment({
+      workspaceId: command.workspaceId,
+      name: command.name,
+      code: command.code,
+      description: command.description,
+      managerId: command.managerId,
+      parentDepartmentId: command.parentDepartmentId,
+    })
+  }
+}
