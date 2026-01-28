3d02e3f845ce9449a316ad0f09f30ca5687149c5
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 09:55:57 +0530

    add GetRuleById query and handler for retrieving category rule by ID

---
+++ modules/categorization-rules/application/queries/get-rule-by-id.query.ts
@@ -0,0 +1,31 @@
+import { CategoryRuleService } from '../services/category-rule.service'
+import { RuleId } from '../../domain/value-objects/rule-id'
+
+export interface GetRuleByIdQuery {
+  ruleId: string
+}
+
+export class GetRuleByIdHandler {
+  constructor(private readonly ruleService: CategoryRuleService) {}
+
+  async execute(query: GetRuleByIdQuery) {
+    const rule = await this.ruleService.getRuleById(RuleId.fromString(query.ruleId))
+
+    return {
+      id: rule.getId().getValue(),
+      workspaceId: rule.getWorkspaceId().getValue(),
+      name: rule.getName(),
+      description: rule.getDescription(),
+      priority: rule.getPriority(),
+      isActive: rule.getIsActive(),
+      condition: {
+        type: rule.getCondition().getType(),
+        value: rule.getCondition().getValue(),
+      },
+      targetCategoryId: rule.getTargetCategoryId().getValue(),
+      createdBy: rule.getCreatedBy().getValue(),
+      createdAt: rule.getCreatedAt(),
+      updatedAt: rule.getUpdatedAt(),
+    }
+  }
+}
