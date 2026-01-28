0971cae27395b1a010aa0c2676a726cb8c1f44cf
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 20:47:38 +0530

    refactor ListUserWorkflowsHandler to return PaginatedResult and improve input structure

--- modules/approval-workflow/application/queries/list-user-workflows.query.ts
+++ modules/approval-workflow/application/queries/list-user-workflows.query.ts
@@ -1,20 +1,27 @@
-import { ExpenseWorkflowRepository } from '../../domain/repositories/expense-workflow.repository'
-import { ExpenseWorkflow } from '../../domain/entities/expense-workflow.entity'
+import { ExpenseWorkflowRepository } from "../../domain/repositories/expense-workflow.repository";
+import { ExpenseWorkflow } from "../../domain/entities/expense-workflow.entity";
+import { PaginatedResult } from "../../../../apps/api/src/shared/domain/interfaces/paginated-result.interface";
 
 export interface ListUserWorkflowsInput {
-  userId: string
-  workspaceId: string
+  userId: string;
+  workspaceId: string;
+  limit?: number;
+  offset?: number;
 }
 
 export class ListUserWorkflowsHandler {
-  constructor(
-    private readonly workflowRepository: ExpenseWorkflowRepository
-  ) {}
+  constructor(private readonly workflowRepository: ExpenseWorkflowRepository) {}
 
-  async handle(input: ListUserWorkflowsInput): Promise<ExpenseWorkflow[]> {
+  async handle(
+    input: ListUserWorkflowsInput,
+  ): Promise<PaginatedResult<ExpenseWorkflow>> {
     return await this.workflowRepository.findByUser(
       input.userId,
-      input.workspaceId
-    )
+      input.workspaceId,
+      {
+        limit: input.limit,
+        offset: input.offset,
+      },
+    );
   }
 }
