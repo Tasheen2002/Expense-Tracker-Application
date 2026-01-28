eb9d509d46f153d1cc8d7d1ce8c0f5e5e462daba
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 20:55:35 +0530

    refactor ListExpensesQuery and ListExpensesHandler to improve pagination and user filtering

--- modules/expense-ledger/application/queries/list-expenses.query.ts
+++ modules/expense-ledger/application/queries/list-expenses.query.ts
@@ -1,17 +1,25 @@
+import { ExpenseService } from "../services/expense.service";
+
 export class ListExpensesQuery {
   constructor(
     public readonly workspaceId: string,
-    public readonly userId?: string
+    public readonly userId?: string,
+    public readonly limit?: number,
+    public readonly offset?: number,
   ) {}
 }
 
 export class ListExpensesHandler {
-  constructor(private readonly expenseService: any) {}
+  constructor(private readonly expenseService: ExpenseService) {}
 
   async handle(query: ListExpensesQuery) {
-    if (query.userId) {
-      return await this.expenseService.getExpensesByUser(query.userId, query.workspaceId)
-    }
-    return await this.expenseService.getExpensesByWorkspace(query.workspaceId)
+    return await this.expenseService.getExpensesWithFilters({
+      workspaceId: query.workspaceId,
+      userId: query.userId,
+      pagination: {
+        limit: query.limit,
+        offset: query.offset,
+      },
+    });
   }
 }
