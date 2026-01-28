683263b8dd0eba39ad9658307ceff8556e5d862c
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 12:43:46 +0530

    add ListForecastsQuery and handler for retrieving forecasts

---
+++ modules/budget-planning/application/queries/list-forecasts.query.ts
@@ -0,0 +1,14 @@
+import { ForecastService } from "../services/forecast.service";
+import { Forecast } from "../../domain/entities/forecast.entity";
+
+export class ListForecastsQuery {
+  constructor(public readonly planId: string) {}
+}
+
+export class ListForecastsHandler {
+  constructor(private readonly forecastService: ForecastService) {}
+
+  async handle(query: ListForecastsQuery): Promise<Forecast[]> {
+    return await this.forecastService.listForecasts(query.planId);
+  }
+}
