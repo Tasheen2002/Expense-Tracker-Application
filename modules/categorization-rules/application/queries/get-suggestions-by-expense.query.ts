5a333c972502a9bedb55d50051ed930a80b8ed7a
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-28 09:56:26 +0530

    add GetSuggestionsByExpense query and handler for retrieving category suggestions by expense ID

---
+++ modules/categorization-rules/application/queries/get-suggestions-by-expense.query.ts
@@ -0,0 +1,31 @@
+import { CategorySuggestionService } from '../services/category-suggestion.service'
+import { ExpenseId } from '../../../expense-ledger/domain/value-objects/expense-id'
+import { WorkspaceId } from '../../../identity-workspace/domain/value-objects/workspace-id.vo'
+
+export interface GetSuggestionsByExpenseQuery {
+  expenseId: string
+  workspaceId: string
+}
+
+export class GetSuggestionsByExpenseHandler {
+  constructor(private readonly suggestionService: CategorySuggestionService) {}
+
+  async execute(query: GetSuggestionsByExpenseQuery) {
+    const suggestions = await this.suggestionService.getSuggestionsByExpenseId(
+      ExpenseId.fromString(query.expenseId),
+      WorkspaceId.fromString(query.workspaceId)
+    )
+
+    return suggestions.map((suggestion) => ({
+      id: suggestion.getId().getValue(),
+      workspaceId: suggestion.getWorkspaceId().getValue(),
+      expenseId: suggestion.getExpenseId().getValue(),
+      suggestedCategoryId: suggestion.getSuggestedCategoryId().getValue(),
+      confidence: suggestion.getConfidence().getValue(),
+      reason: suggestion.getReason(),
+      isAccepted: suggestion.getIsAccepted(),
+      createdAt: suggestion.getCreatedAt(),
+      respondedAt: suggestion.getRespondedAt(),
+    }))
+  }
+}
