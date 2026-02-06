762ff71f8b567784ceeeb90a716ea8dc91f45bcd
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-06 13:19:12 +0530

    refactor ListCategoriesHandler to enforce return type and improve code formatting

--- modules/expense-ledger/application/queries/list-categories.query.ts
+++ modules/expense-ledger/application/queries/list-categories.query.ts
@@ -1,17 +1,23 @@
+import { PaginatedResult } from "../../../../apps/api/src/shared/domain/interfaces/paginated-result.interface";
+
 export class ListCategoriesQuery {
   constructor(
     public readonly workspaceId: string,
-    public readonly activeOnly: boolean = false
+    public readonly activeOnly: boolean = false,
   ) {}
 }
 
 export class ListCategoriesHandler {
   constructor(private readonly categoryService: any) {}
 
-  async handle(query: ListCategoriesQuery) {
+  async handle(query: ListCategoriesQuery): Promise<PaginatedResult<any>> {
     if (query.activeOnly) {
-      return await this.categoryService.getActiveCategoriesByWorkspace(query.workspaceId)
+      return await this.categoryService.getActiveCategoriesByWorkspace(
+        query.workspaceId,
+      );
     }
-    return await this.categoryService.getCategoriesByWorkspace(query.workspaceId)
+    return await this.categoryService.getCategoriesByWorkspace(
+      query.workspaceId,
+    );
   }
 }
