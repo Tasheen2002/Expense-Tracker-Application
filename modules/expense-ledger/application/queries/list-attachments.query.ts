375085a803ba80bc7e04cd7adc363c018c1e30c7
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-06 18:08:11 +0530

    refactor list attachments query to return paginated results

--- modules/expense-ledger/application/queries/list-attachments.query.ts
+++ modules/expense-ledger/application/queries/list-attachments.query.ts
@@ -1,15 +1,16 @@
-import { AttachmentService } from '../services/attachment.service'
-import { Attachment } from '../../domain/entities/attachment.entity'
+import { AttachmentService } from "../services/attachment.service";
+import { Attachment } from "../../domain/entities/attachment.entity";
+import { PaginatedResult } from "../../../../apps/api/src/shared/domain/interfaces/paginated-result.interface";
 
 export interface ListAttachmentsDto {
-  expenseId: string
-  workspaceId: string
+  expenseId: string;
+  workspaceId: string;
 }
 
 export class ListAttachmentsHandler {
   constructor(private readonly attachmentService: AttachmentService) {}
 
-  async handle(dto: ListAttachmentsDto): Promise<Attachment[]> {
-    return await this.attachmentService.getAttachmentsByExpense(dto.expenseId)
+  async handle(dto: ListAttachmentsDto): Promise<PaginatedResult<Attachment>> {
+    return await this.attachmentService.getAttachmentsByExpense(dto.expenseId);
   }
 }
