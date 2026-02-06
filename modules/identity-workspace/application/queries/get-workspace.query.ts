5cd9bd65f1f7a9045ca05718ffe07766f3c5a104
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-06 13:21:09 +0530

    refactor get-workspace queries to improve code formatting and enforce consistent use of semicolons

--- modules/identity-workspace/application/queries/get-workspace.query.ts
+++ modules/identity-workspace/application/queries/get-workspace.query.ts
@@ -1,27 +1,35 @@
-import { WorkspaceManagementService } from '../services/workspace-management.service'
-import { Workspace } from '../../domain/entities/workspace.entity'
+import { WorkspaceManagementService } from "../services/workspace-management.service";
+import { Workspace } from "../../domain/entities/workspace.entity";
 
 export interface GetWorkspaceByIdQuery {
-  workspaceId: string
+  workspaceId: string;
 }
 
 export interface GetUserWorkspacesQuery {
-  userId: string
+  userId: string;
 }
 
 export class GetWorkspaceByIdHandler {
-  constructor(private readonly workspaceManagementService: WorkspaceManagementService) {}
+  constructor(
+    private readonly workspaceManagementService: WorkspaceManagementService,
+  ) {}
 
   async handle(query: GetWorkspaceByIdQuery): Promise<Workspace | null> {
-    return await this.workspaceManagementService.getWorkspaceById(query.workspaceId)
+    return await this.workspaceManagementService.getWorkspaceById(
+      query.workspaceId,
+    );
   }
 }
 
 export class GetUserWorkspacesHandler {
-  constructor(private readonly workspaceManagementService: WorkspaceManagementService) {}
+  constructor(
+    private readonly workspaceManagementService: WorkspaceManagementService,
+  ) {}
 
   async handle(query: GetUserWorkspacesQuery): Promise<Workspace[]> {
     // FIXED: Get ALL workspaces user is a member of, not just owned workspaces
-    return await this.workspaceManagementService.getWorkspacesByMembership(query.userId)
+    return await this.workspaceManagementService.getWorkspacesByMembership(
+      query.userId,
+    );
   }
 }
