19fbc2d79595509c376f42a80dae8ba77c07264b
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-06-16 00:25:17 +0530

    Refactor import paths in repository interfaces for consistency

--- modules/receipt-vault/domain/repositories/receipt-tag-definition.repository.ts
+++ modules/receipt-vault/domain/repositories/receipt-tag-definition.repository.ts
@@ -4,7 +4,7 @@ import { TagId } from "../value-objects/tag-id";
 import {
   PaginatedResult,
   PaginationOptions,
-} from '../../../../packages/core/src/domain/interfaces/paginated-result.interface';
+} from '@core/domain/interfaces/paginated-result.interface';
 
 export interface IReceiptTagDefinitionRepository {
   save(tag: ReceiptTagDefinition): Promise<void>;
--- modules/receipt-vault/domain/repositories/receipt.repository.ts
+++ modules/receipt-vault/domain/repositories/receipt.repository.ts
@@ -5,7 +5,7 @@ import { ReceiptType } from "../enums/receipt-type";
 import {
   PaginatedResult,
   PaginationOptions,
-} from '../../../../packages/core/src/domain/interfaces/paginated-result.interface';
+} from '@core/domain/interfaces/paginated-result.interface';
 
 export interface ReceiptFilters {
   workspaceId: string;
