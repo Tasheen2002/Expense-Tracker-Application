f2c228235fb17f327775f65d7464502a36251d6e
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-06-18 12:52:37 +0530

    Refactor import path for DomainError to use absolute path for consistency

--- modules/event-outbox/domain/errors/outbox-event.errors.ts
+++ modules/event-outbox/domain/errors/outbox-event.errors.ts
@@ -1,4 +1,4 @@
-import { DomainError } from '../../../../packages/core/src/domain/domain-error';
+import { DomainError } from '@core/domain/domain-error';
 
 export class OutboxEventError extends DomainError {
   constructor(message: string, code: string, statusCode: number) {
