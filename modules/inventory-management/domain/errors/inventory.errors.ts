166024b4e940734fdea68b78cbbf0819f4836d42
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-06-19 12:19:43 +0530

    Refactor import path in inventory.errors.ts to use absolute path for consistency

--- modules/inventory-management/domain/errors/inventory.errors.ts
+++ modules/inventory-management/domain/errors/inventory.errors.ts
@@ -1,4 +1,4 @@
-import { DomainError } from '../../../../packages/core/src/domain/domain-error';
+import { DomainError } from '@core/domain/domain-error';
 
 export class InventoryManagementError extends DomainError {
   constructor(message: string, code: string, statusCode: number = 400) {
