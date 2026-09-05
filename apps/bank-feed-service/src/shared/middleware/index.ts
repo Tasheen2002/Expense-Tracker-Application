1f477bdae6e5c3f6f3b22b02ade4a18e2a12f936
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-09-05 10:57:56 +0530

    Remove files

--- apps/bank-feed-service/src/shared/middleware/index.ts
+++
@@ -1,5 +0,0 @@
-export { authenticate } from "./authenticate.middleware";
-export { optionalAuth } from "./optional-auth.middleware";
-export { workspaceAuthorizationMiddleware } from "./workspace-authorization.middleware";
-export { requireRole, RolePermissions, hasRole } from "./role-authorization.middleware";
-
